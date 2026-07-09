37e7c0ea7f4ccb84e2ef2e481bdf5ed05186f74a
Turca Man <280507438+turcaman@users.noreply.github.com>
2026-07-08 19:18:47 -0600

    feat: update domain from turcaweb.pages.dev to turcanime.pages.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://turcaweb.pages.dev',
+  site: 'https://turcanime.pages.dev',
   integrations: [],
   vite: {
     plugins: [tailwindcss()]
