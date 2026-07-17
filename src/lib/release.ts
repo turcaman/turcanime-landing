092b3f01532e0d561baf100bed8e7bb2b323f602
Turca Man <280507438+turcaman@users.noreply.github.com.>
2026-07-16 18:44:15 -0600

    chore: bump version to android 1.10.7 / desktop 1.0.6

--- src/lib/release.ts
+++ src/lib/release.ts
@@ -12,8 +12,5 @@ export const release = {
       debUrl: "https://github.com/turcaman/turcanime-desktop/releases/download/v1.0.6/Turcanime-1.0.6-linux-x64.deb",
       rpmUrl: "https://github.com/turcaman/turcanime-desktop/releases/download/v1.0.6/Turcanime-1.0.6-linux-x64.rpm",
     },
-    macos: {
-      available: false,
-    },
   },
 } as const;
